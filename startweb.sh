de044c7fed35d54f2dcc3fe37ba837cb87b361b3
Eli McRae <shyft@shyft.us>
2022-12-06 12:00:21 -0600

    fixing validator crashes with gunicorn

--- startweb.sh
+++ startweb.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-ls -R *.py|  entr -r python ./challenge_validator_webapp.py &
-ls -R *.py|  entr -r python ./scoreboard.py &
+#ls -R *.py|  entr -r python ./challenge_validator_webapp.py &
+#ls -R *.py|  entr -r python ./scoreboard.py &
+ls -R *.py|  entr -r gunicorn --bind 127.0.0.1:5000 challenge_validator_webapp:app &
+ls -R *.py|  entr -r gunicorn --bind 127.0.0.1:4000 scoreboard:app &
 
