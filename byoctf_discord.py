c886258ee0141a42ff50f605eb380cac4339862b
Eli McRae <shyft@shyft.us>
2023-05-12 13:11:03 -0500

    formatting for viewing all challenges

--- byoctf_discord.py
+++ byoctf_discord.py
@@ -127,13 +127,13 @@ async def getDiscordUser(ctx, target_user: str):
 
 
 async def sendBigMessage(ctx, content, wrap=True):
-    """this should split a long message across multiple sends if it exceeds the 2000 char limit of discord. requires newlines in the message. Set wrap=False to omit the code blocks for your message"""
+    """this should split a long message across multiple sends if it exceeds the 4000 char limit of discord. requires newlines in the message. Set wrap=False to omit the code blocks for your message"""
     lines = content.splitlines(keepends=True)
     if SETTINGS["_debug"] == True and SETTINGS["_debug_level"] > 1:
         logger.debug(content)
     chunk = ""
     for line in lines:
-        if len(chunk) + len(line) < 1600:
+        if len(chunk) + len(line) < 3600:
             chunk = chunk + line
         else:
             if wrap:
@@ -899,7 +899,7 @@ async def list_all(ctx, *, tags=None):
     res.insert(0, ["ID", "Author", "Title", "Total Value", "Done", "Rating", "Tags"])
     table = GithubFlavoredMarkdownTable(res)
     # logger.debug("discord",challs)
-    msg = f"Showing all unlocked challenges```{table.table}```"
+    msg = f"Showing all unlocked challenges\n```{table.table}```"
     await sendBigMessage(ctx,msg)
     # await ctx.send(msg)
 
