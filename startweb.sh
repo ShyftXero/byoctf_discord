cd1f99e6acdb41f7a3fb0c89a62742c0c9fcd6e8
Eli McRae <shyft@shyft.us>
2023-12-26 11:55:13 -0600

    updates

--- startweb.sh
+++ startweb.sh
@@ -15,12 +15,11 @@ APP_DIR="/root/byoctf_discord"
 LOG_DIR="$APP_DIR/logs"
 
 # Ensure the application and log directories exist
-mkdir -p "$APP_DIR"
-mkdir -p "$LOG_DIR"
+[ -d "$APP_DIR" ] || mkdir -p "$APP_DIR"
+[ -d "$LOG_DIR" ] || mkdir -p "$LOG_DIR"
 
 # Check if supervisord is installed and install it if it's not
-if ! command -v supervisord &> /dev/null
-then
+if ! command -v supervisord &> /dev/null; then
     echo "supervisord could not be found, installing now..."
     apt-get update && apt-get install -y supervisor
 else
@@ -65,12 +64,27 @@ stdout_logfile=$LOG_DIR/scoreboard.out.log
 [group:appgroup]
 programs=challenge_validator_webapp,scoreboard"
 
-# Save the supervisord configuration to a file
-echo "$SUPERVISORD_CONF" > /etc/supervisor/conf.d/byoctf_discord.conf
+# Check if the configuration file already exists and matches the desired state
+CONF_FILE="/etc/supervisor/conf.d/byoctf_discord.conf"
+if [ -f "$CONF_FILE" ]; then
+    # Check if the existing file content matches the desired configuration
+    if ! echo "$SUPERVISORD_CONF" | diff - "$CONF_FILE" > /dev/null; then
+        # If it doesn't match, update the configuration
+        echo "$SUPERVISORD_CONF" > "$CONF_FILE"
+        echo "Updated the existing supervisord configuration."
+    else
+        echo "The supervisord configuration is already up-to-date."
+    fi
+else
+    # If the configuration file doesn't exist, create it
+    echo "$SUPERVISORD_CONF" > "$CONF_FILE"
+    echo "Created a new supervisord configuration."
+fi
 
 # Reload supervisord configuration and update the programs
+# This is generally safe to run multiple times and is necessary if the configuration has changed
 supervisorctl reread
 supervisorctl update
 
-# Start all programs
+# Start all programs (only start those that are not already running)
 supervisorctl start all
