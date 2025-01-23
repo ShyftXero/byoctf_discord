fd4e2bd44b1f1e11d373c0c82dd7a33c60128ca4
Matthew Moses <8397647+matt-moses@users.noreply.github.com>
2025-01-22 23:12:21 -0600

    Potential fix for non-admin grant of points.

--- scoreboard.py
+++ scoreboard.py
@@ -350,6 +350,7 @@ def buy_hint(hint_uuid):
     return resp
 
 @app.post("/api/grant_points")
+@get_admin_api_key
 @db.db_session
 def grant_points():
     """
@@ -379,7 +380,7 @@ def grant_points():
         return "admin_api_key missing from payload", 405
     # did they present the correct one?
     admin_user = db.get_user_by_api_key(api_key)
-    if admin_user == None:
+    if admin_user == None or not admin_user.is_admin:
         return "invalid admin api key", 403
 
     # seems legit...
